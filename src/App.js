42dd3f2810fbf7d2f3db7cb5dd6ae74b2e443df8
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-08-18 15:02:23 -0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
 import ProvinceDetails from './Pages/Detials';
+import Home from './Pages/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => (
