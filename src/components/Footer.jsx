0f5b872ffad3c9c9c532edab81298398f4ce0721
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2024-08-15 21:51:44 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => (
   <div className="card-footer text-body-secondary">
     <footer>
       ©
-      <a href="https://github.com/MasumaJaffery" style={{ color: '#fff' }}>@MasumaJaffery</a>
+      <a href="masumajaffery1@gmail.com" style={{ color: '#fff' }}>@MasumaJaffery</a>
       Today&apos;s Weather-2023
     </footer>
   </div>
